92964d55fad8c001742127cc2c7fb7a80b196f39
RupomChowdhury <web.rupom@gmail.com>
2026-01-10 14:31:29 +0600

    Input Validation on DTO - 10/01/2026

--- src/main/java/dev/rupom/learning/springboot/webapi/dto/EmployeeDTO.java
+++ src/main/java/dev/rupom/learning/springboot/webapi/dto/EmployeeDTO.java
@@ -1,9 +1,12 @@
 package dev.rupom.learning.springboot.webapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
+import jdk.jfr.BooleanFlag;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDate;
 
@@ -12,10 +15,32 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class EmployeeDTO {
     private Long id;
+
+    @NotBlank(message = "Name is required.")
+    @Size(min = 5, max = 30, message = "Number of characters in name should be in range: [5-30]")
     private String name;
+
+    @Email(message = "Invalid email format.")
     private String email;
+
+    @Min(value = 18,message = "Age should be greater than 18")
+    @Max(value = 60,message = "Age should be less than 60")
     private Integer age;
+
 //    @JsonProperty("joiningDate")
+    @NotNull @PastOrPresent
     private LocalDate dateOfJoining;
+
+    @NotBlank(message = "Role is required.")
+    @Pattern(regexp = "^(admin|user)$",message = "Invalid role.")
+    private String role;
+
+    @NotNull @Positive(message = "Salary should be positive.")
+    @Digits(integer = 6, fraction = 2,message = "Invalid salary format: [XXXXXX.XX]")
+    @DecimalMax(value = "100000.99",message = "Salary should be less than 100000.99")
+    @DecimalMin(value = "1000.59",message = "Salary should be greater than 1000.00")
+    private Double salary;
+
+    @AssertTrue(message = "Employee should be active.")
     private Boolean isActive;
 }
